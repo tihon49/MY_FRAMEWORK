d8df18f8cd5afbaefee342b96b34914f387971b1
tihon49 <a.tihonov@aerodisk.ru>
2022-05-03 14:13:59 +0300

    rich added

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import re
 
 
-def get_menu_numbers(menu_data: str):
+def get_menu_numbers(menu_data: str) -> list:
+    """возвращает номера пунктов мню в виде списка int()"""
     pattern = re.compile('\d. ')
     result = pattern.findall(menu_data)
     result = [int(i.replace('. ', '')) for i in result]
